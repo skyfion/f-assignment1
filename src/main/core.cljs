7e674b5035bdf079bae5d89355b255a6698d4ff4
Aleksandr Kondratyev <aleksandr.kondratyev@suprematic.net>
2021-12-03 23:44:32 +0500

    init empty template

---
+++ src/main/core.cljs
@@ -0,0 +1,9 @@
+(ns core
+  (:require [re-frame.core :as re-frame]
+            [reagent.core :as reagent]
+            [views :as views]))
+
+(defn ^:export init
+  []
+  (re-frame/dispatch-sync [:initialise-db])
+  (reagent/render [views/app] (.getElementById js/document "app")))
---
+++ src/main/views.cljs
@@ -0,0 +1,6 @@
+(ns views)
+
+
+(defn app
+  []
+  [:div "TODO"])
