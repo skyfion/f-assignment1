b4ec9d84143261cf53dd592d56ab793744fe268d
Aleksandr Kondratyev <aleksandr.kondratyev@suprematic.net>
2021-12-05 22:49:38 +0500

    fix hot reload

--- src/main/core.cljs
+++ src/main/core.cljs
@@ -4,7 +4,11 @@
             [views :as views]
             [events]))
 
+(defn ^:dev/after-load mount
+  []
+  (reagent/render [views/app] (.getElementById js/document "app")))
+
 (defn ^:export init
   []
   (re-frame/dispatch-sync [:initialise-db])
-  (reagent/render [views/app] (.getElementById js/document "app")))
+  (mount))
